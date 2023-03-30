fc23bd5a6d9d68e4fef93183a297487be19b31fd
Protogenic <vladimirbezrokov@gmail.com>
2023-03-30 16:17:47 +0300

    Display categories, add new

--- src/main/java/com/example/characterdevelopment/controller/HomepageController.java
+++ src/main/java/com/example/characterdevelopment/controller/HomepageController.java
@@ -1,2 +1,39 @@
-package com.example.characterdevelopment.controller;public class HomepageController {
+package com.example.characterdevelopment.controller;
+
+import com.example.characterdevelopment.dao.CategoryDAO;
+import com.example.characterdevelopment.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/home")
+public class HomepageController {
+
+    private CategoryDAO categoryDAO;
+
+    @Autowired
+    public HomepageController(CategoryDAO categoryDAO) {
+        this.categoryDAO = categoryDAO;
+    }
+
+    @GetMapping()
+    public String homePage(Model model) {
+        model.addAttribute("categories", categoryDAO.findAll());
+        return "home";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("category", new Category());
+        return "add";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("category") Category category) {
+        categoryDAO.save(category);
+        return "redirect:/home";
+    }
+
 }
--- src/main/java/com/example/characterdevelopment/model/Category.java
+++ src/main/java/com/example/characterdevelopment/model/Category.java
@@ -1,2 +1,46 @@
-package com.example.characterdevelopment.model;public class Category {
+package com.example.characterdevelopment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int level;
+
+    public Category() {
+    }
+
+    public Category(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
